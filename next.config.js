bcde83f91b93c504d5fa22f331df3b37d42f7b41
Pauli <Pauli@365evergreen.com>
2026-01-19 11:19:35 +1000

    feat: add image remote patterns for hiredau.com in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hiredau.com',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
